c6f2139b41f4d1321bb2986284fb19db6ec6d37f
mil-github-048 <milindbebarta97@gmail.com>
2022-03-01 01:02:06 +0530

    Commit 2 : Adding libraries (readr and dplyr) and filtering the data dataframe into a new dataframe data1

--- Chart_1.R
+++ Chart_1.R
@@ -1,3 +1,16 @@
+library(readr)
+library(dplyr)
+
 data <- read.csv2("household_power_consumption.txt")
 
 head(data)
+
+# Converting the date column from character to date type
+
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+# Taking the data that is required
+
+data1 <- data %>% 
+         filter(Date >= as.Date('2007-02-01') & Date <= as.Date('2007-02-02'))
+head(data1)
