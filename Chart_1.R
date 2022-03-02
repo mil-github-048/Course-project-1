f2e96096dde197551d5a70c8e7f6e5ea60cc094f
mil-github-048 <milindbebarta97@gmail.com>
2022-03-02 23:46:00 +0530

    Final commit chart 1

--- Chart_1.R
+++ Chart_1.R
@@ -13,4 +13,18 @@ data$Date <- as.Date(data$Date, "%d/%m/%Y")
 
 data1 <- data %>% 
          filter(Date >= as.Date('2007-02-01') & Date <= as.Date('2007-02-02'))
-head(data1)
+
+
+# Making a column that contains both date and time
+
+new_date <- paste(data1$Date, data1$Time)
+
+data1 <- cbind(new_date, data1)
+
+data1$new_date <- as.POSIXct(new_date)
+
+# changing the type of Global active power from char to num
+data1$Global_active_power <- as.numeric(data1$Global_active_power)
+
+# Making the histogram plot
+hist(data1$Global_active_power, main='Global Active Power', xlab='Global Active Power(kilowatts)', col='red')
