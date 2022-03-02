6c37939cc5f0c3d07dc62456517d6fc0a39b10a9
mil-github-048 <milindbebarta97@gmail.com>
2022-03-03 00:23:13 +0530

    final commit plot 4

---
+++ Chart_4.R
@@ -0,0 +1,47 @@
+library(readr)
+library(dplyr)
+
+data <- read.csv2("household_power_consumption.txt")
+
+head(data)
+
+# Converting the date column from character to date type
+
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+# Taking the data that is required
+
+data1 <- data %>% 
+  filter(Date >= as.Date('2007-02-01') & Date <= as.Date('2007-02-02'))
+
+
+# Making a column that contains both date and time
+
+new_date <- paste(data1$Date, data1$Time)
+
+data1 <- cbind(new_date, data1)
+
+data1$new_date <- as.POSIXct(new_date)
+
+# changing the type of Global active power from char to num
+data1$Global_active_power <- as.numeric(data1$Global_active_power)
+
+
+
+
+
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data1, {
+  plot(Global_active_power~new_date, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~new_date, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~new_date, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~new_date,col='Red')
+  lines(Sub_metering_3~new_date,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~new_date, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
